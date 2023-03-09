48979ee7724dd363103843caa860e7a2cef6144e
charles smith <char_smith@hotmail.com>
2023-03-08 21:19:18 -0500

    t1

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // API à utiliser pour les images de chiens: https://dog.ceo/api/breeds/image/random
+
+
+
+fetch('https://dog.ceo/api/breeds/image/random')
+
+.then ((response)=> {
+    return response.text()
+})
+.then ((text)=>{
+    console.log(text)
+})
+

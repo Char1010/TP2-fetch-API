8e3fcf7b6209be11bb97c5add4b93c6fa5c7d428
charles smith <char_smith@hotmail.com>
2023-03-16 18:28:24 -0400

    revision javascrpt

--- app.js
+++ app.js
@@ -8,49 +8,26 @@ let dogAge = document.getElementById('dogAge');
 let dogRegistry = document.getElementById('dogRegistry');
 
 btn.addEventListener('click',() =>{
-fetch(url)
-    .then(response => response.json())
-    .then(data=> {
-    console.log(data)
-    image.src = data.message
-    dogId.innerHTML = '${data.file}'
-})
-    .catch(error=>console.log(error))
-})
+    fetch(url)
+        .then(response => response.json())
+        .then(data=> {
+        console.log(data)
+        image.src = data.message
+        dogId.innerHTML = '${data.file}'
+    })
+        .catch(error=>console.log(error))
+    })
 
-/**
-const cat_result = document.getElementById( 'dogId' );
-const dog_result = document.getElementById( 'dogAge' );
-const fox_result = document.getElementById( 'dogRegistry' );
+function displaydog(dog){
+    const dogId = document.getElementById( 'dogId' );
+    const dogAge = document.getElementById( 'dogAge' );
+    const dogRegistry = document.getElementById( 'dogRegistry' );
+    const image = document.getElementById( 'dogsAPI' );
 
+    dogId.innerText = '${dog.id}';
+    dogAge.innerText = '${dog.age}';
+    dogRegistry.innerText ='${dog.registry}';
+    image.setAttribute('src','${dog.picture.large}');
 
-cat_btn.addEventListener( 'click', getRandomCat );
-dog_btn.addEventListener( 'click', getRandomDog );
-fox_btn.addEventListener( 'click', getRandomFox );
-
-function getRandomCat() {
-	fetch( 'https://aws.random.cat/meow' )
-		.then( res => res.json() )
-		.then( data => {
-			cat_result.innerHTML = `<img src="${data.file}" />`;
-		} );
 }
-**/
-
-/** 
-let url= 'https://dog.ceo/api/breeds/image/random';
-
-fetch(url).then((data) =>{
-    return data.json ();
-}).then((completedata)=>{
-   // console.log(completedata);
-   let data1='';
-   completedata.map((values)=>{
-data1
-   })
-
-}).catch((error)=>{
-    console.log(err);
-})
-**/
-
+getDog();
